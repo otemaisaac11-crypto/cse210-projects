4219cbd886dd3e9aebe7bc89e341e7f8917747db
otemaisaac11-crypto <otemaisaac11@gail.com>
2026-03-07 14:13:49 +0300

    Lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+       
+        Console.WriteLine("Enter numbers (or 0 to stop):"); 
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number:");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int  number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is {largest}");
+
+
+
     }
 }
