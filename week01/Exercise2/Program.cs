50ae9d14cff206e37b392725af05336c6f18cd3f
otemaisaac11-crypto <otemaisaac11@gail.com>
2026-03-07 09:11:39 +0300

    Conditions

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A.");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine ("Your grade is B.");
+        }
+
+        else if (grade >= 70)
+        {
+           Console.WriteLine("Your grade is C.");    
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D.");
+        }
+
+        else
+        {
+            Console.WriteLine("Your grade is F.");
+        }
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you can try again next semester.");
+        }
     }
+
+
 }
