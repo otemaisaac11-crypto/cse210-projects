127039f4ff5e68d26a1c665da0f2664d388a95c9
otemaisaac11-crypto <otemaisaac11@gail.com>
2026-03-14 14:35:34 +0300

    Resume Assignment

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 My name is Isaac Otema
 
 This repository contains the starter code for many different projects. They are arranged folders by week. There is also an an empty sandbox project that can be used to play around with any concept you would like.
+
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+         Job job2 = new Job();
+        job2._company = "Bing";
+        job2._jobTitle = "Marketing Engineer";
+        job2._startYear = 2019;
+        job2._endYear = 2024;
+
+     // Create resume
+        Resume myResume = new Resume();
+        myResume._name = "Isaac Kevin";
+
+     // Adding jobs to resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
     }
+    
 }
